16c8540aaaee24fda0a1cfa4572fa3a03925757e
david švancar <dejvisvan@seznam.cz>
2022-10-31 09:56:00 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,13 +1,15 @@
 const send = document.getElementById("send");
 const userInput = document.getElementById("userInput");
-const send2 = document.getElementById("send2");
-const userInput2 = document.getElementById("userInput2");
-const content = document.getElementById("content")
+
+
 
 send.onclick = () => {
     document.body.style.backgroundImage = `url(${userInput.value})`;
 }
 
+const send2 = document.getElementById("send2");
+const userInput2 = document.getElementById("userInput2");
+const content = document.getElementById("content");
 
 send2.onclick = () => {
     content.style.fontSize = userInput2.value;
